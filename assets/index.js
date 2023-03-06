cdec902c3f0fe7f893b7845a878e50996fed7706
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2023-03-06 17:37:04 +0530

    created a new bare workflow react native project

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+const assets = {
+  lottieFiles: {
+    login: require('./login.json'),
+    temporary: require('./temporary.json'),
+    signup: require('./signup.json'),
+  },
+};
+export default assets;
